deae2c367e6859aac944c84d484ae869426538d1
ds759 <ds759@bath.ac.uk>
2020-05-10 18:43:12 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/users", users);
-app.use("/albums", albums);
-app.use("/logins", logins);
-app.use("/plays", plays);
+app.use("/api/albums", albums);
+app.use("/api/logins", logins);
+app.use("/api/plays", plays);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
