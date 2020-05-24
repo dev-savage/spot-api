cbb61572712611f0fba2b75f7199ca178a80055e
ds759 <ds759@bath.ac.uk>
2020-05-24 22:16:22 +0100

    lots

--- backend/routes/virtualmachines.js
+++ backend/routes/virtualmachines.js
@@ -2,18 +2,12 @@ const express = require("express");
 const router = express.Router();
 const db = require("../utils/database");
 
-router.post("/start", async (req, res, next) => {
-	const ip = req.body.ip;
-	const user = req.body.user;
-	const pass = req.body.pass;
-	const name = req.body.name;
-	const result = await ls();
-	res.send(result);
-});
-
-router.post("/end", (req, res, next) => {
+router.post("/status", (req, res, next) => {
+	const vm = req.body.vm;
+	const status = req.body.status;
+	const sql = `UPDATE spotify.vm SET status='${status}' WHERE vm='${vm}';`;
 	db.pool.getConnection(function (err, connection) {
-		connection.query(insertError(req), function (error, results, fields) {
+		connection.query(sql, function (error, results, fields) {
 			if (error) throw error;
 			connection.release();
 			res.send(results);
--- my-app/src/views/VirtualMachines.jsx
+++ my-app/src/views/VirtualMachines.jsx
@@ -1,5 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { Card, CardHeader, CardBody, CardTitle, Row, Col } from "reactstrap";
+import React, { useState, useEffect, useRef } from "react";
+import {
+	Card,
+	CardHeader,
+	CardBody,
+	CardTitle,
+	Row,
+	Col,
+	Button,
+} from "reactstrap";
 import fontawesome from "@fortawesome/fontawesome";
 import {
 	faPlus,
@@ -13,16 +21,6 @@ import "../css/Card.css";
 import "../css/VirtualMachines.css";
 import RingLoader from "react-spinners/RingLoader";
 import axios from "axios";
-import {
-	Card as CC,
-	CardContent,
-	Grid,
-	Typography,
-	Avatar,
-} from "@material-ui/core";
-import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
-import MoneyIcon from "@material-ui/icons/Money";
-import clsx from "clsx";
 
 fontawesome.library.add(faPlus, faTrashAlt, faPencilAlt, faFileExcel);
 
@@ -58,9 +56,9 @@ const VirtualMachines = () => {
 						<CardHeader>
 							<Row>
 								<Col className="text-left" sm="6">
-									<h5 className="card-category">Overall</h5>
+									<h5 className="card-category">Virtual Machines</h5>
 									<CardTitle className="header--symbol" tag="h4">
-										<h1>VM Contol</h1>
+										Virtual Machines Overview
 									</CardTitle>
 								</Col>
 							</Row>
@@ -69,7 +67,7 @@ const VirtualMachines = () => {
 							{error ? (
 								<h3>Error {error}</h3>
 							) : vm ? (
-								vm.map((element, i) => <VM key={i} machine={element} />)
+								vm.map((element, i) => <VM2 key={i} machine={element} />)
 							) : (
 								<Loader />
 							)}
@@ -80,87 +78,123 @@ const VirtualMachines = () => {
 		</div>
 	);
 };
-const useStyles = makeStyles((theme) => ({
-	root: {
-		height: "100%",
-		// width: "90%",
-		margin: "1.5em",
-		background: "linear-gradient(0deg, #3358f4, #1d8cf8);",
-	},
-	content: {
-		alignItems: "center",
-		display: "flex",
-	},
-	title: {
-		fontWeight: 700,
-	},
-	avatar: {
-		backgroundColor: "white",
-		height: 56,
-		width: 56,
-	},
-	icon: {
-		height: 32,
-		width: 32,
-	},
-	difference: {
-		marginTop: "3em",
-		display: "flex",
-		alignItems: "center",
-	},
-	differenceIcon: {
-		color: "black",
-	},
-	differenceValue: {
-		color: "black",
-		marginRight: "3em",
-	},
-}));
 
-const VM = (props, machine) => {
-	console.log(props);
+const VM2 = (props) => {
+	const formatDate = (newDate) => {
+		let date = newDate.split("T")[0];
+		let time = newDate.split("T")[1];
+		let seconds = time.split(".")[0];
+		return date + " @ " + seconds;
+	};
+
 	const { className, ...rest } = props;
 	const [data, setData] = useState([]);
-	const classes = useStyles();
+	const [date, setDate] = useState(formatDate(props.machine.last_play));
+	const [status, setStatus] = useState(props.machine.status);
+	const [loadingStatus, setLoadingStatus] = useState(false);
+	const vmStyle = {
+		width: "100%",
+	};
 
-	useEffect(() => {
-		console.log("loaded");
-	}, []);
+	useInterval(() => {
+		axios
+			.get(
+				`http://77.68.118.54/api/virtualmachines?vm=${props.machine.hostname}`
+			)
+			.then((res) => {
+				let d = formatDate(res.data.last_play);
+				setDate(d);
+			})
+			.catch((err) => {});
+	}, 10000);
 
+	const toggleStatus = (s) => {
+		setLoadingStatus(true);
+		axios
+			.post(`http://77.68.118.54/api/virtualmachines/status`, {
+				vm: props.machine.vm,
+				status: s,
+			})
+			.then((res) => {
+				setStatus(s);
+				setLoadingStatus(false);
+			})
+			.catch((err) => {});
+	};
 	return (
-		<CC {...rest} className={clsx(classes.root, className)}>
-			<CardContent>
-				<Grid container justify="space-between">
-					<Grid item>
-						<Typography
-							className={classes.title}
-							color="textSecondary"
-							gutterBottom
-							variant="body2"
-						>
-							machine
-						</Typography>
-						<Typography variant="h3">{data}</Typography>
-					</Grid>
-					<Grid item>
-						<Avatar className={classes.avatar}>
-							<MoneyIcon className={classes.icon} />
-						</Avatar>
-					</Grid>
-				</Grid>
-				<div className={classes.difference}>
-					<ArrowDownwardIcon className={classes.differenceIcon} />
-					<Typography className={classes.differenceValue} variant="body2">
-						12%
-					</Typography>
-					<Typography className={classes.caption} variant="caption">
-						Since last month
-					</Typography>
+		<div style={vmStyle} className="content">
+			<Row>
+				<div className="col-12 my--card--inner">
+					<Card className="my--card--stuff inner__card">
+						<CardHeader>
+							<Row>
+								<Col className="text-left" sm="6">
+									<h5 className="card-category text__color">Hostname</h5>
+									<CardTitle className="header--symbol" tag="h4">
+										{props.machine.vm}
+									</CardTitle>
+								</Col>
+							</Row>
+						</CardHeader>
+						<CardBody>
+							<div className="vm__content__container">
+								<div className="info">
+									<br />
+									<h5 className="text__color">Details</h5>
+									<h6 className="text__color">Last Play: {date}</h6>
+									<h6 className="text__color">
+										Today's Plays: {props.machine.count}{" "}
+									</h6>
+									<br />
+									<br />
+									<h6 className="text__color">
+										Login: {props.machine.username} / {props.machine.password}
+									</h6>
+								</div>
+								<div className="actions">
+									<br />
+
+									<h5 className="text__color">Actions</h5>
+									{loadingStatus ? (
+										<Button color="secondary">Pending</Button>
+									) : status ? (
+										<Button color="danger" onClick={() => toggleStatus(false)}>
+											Stop Machine
+										</Button>
+									) : (
+										<Button color="success" onClick={() => toggleStatus(true)}>
+											Start Machine
+										</Button>
+									)}
+								</div>
+							</div>
+						</CardBody>
+					</Card>
 				</div>
-			</CardContent>
-		</CC>
+			</Row>
+		</div>
 	);
 };
+
+function useInterval(callback, delay) {
+	const savedCallback = useRef();
+
+	// Remember the latest function.
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+
+	// Set up the interval.
+	useEffect(() => {
+		function tick() {
+			savedCallback.current();
+		}
+		if (delay !== null) {
+			let id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay]);
+}
 
 const Loader = () => {
 	return (
